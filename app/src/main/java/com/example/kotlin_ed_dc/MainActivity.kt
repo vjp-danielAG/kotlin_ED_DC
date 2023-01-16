eac296bc41069c420ea95eb41c509efe03cad050
Daniel Vitaliano Aedo Gutiérrez <dvaedog01@informatica.iesvalledeljerteplasencia.es>
2023-01-16 20:07:20 +0100

    CM Primeros ejemplos de Kotlin

--- app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.kotlin_ed_dc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    val Bsumar = findViewById<Button>(R.id.bSumar);
+    val bSumar = findViewById<Button>(R.id.bSumar);
+    val tvContador = findViewById<TextView>(R.id.tvMostrarValor);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvContador.text = "Contador: 0";
+
+        bSumar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("CLICKS:","PRUEBA")
+            }
+        })
     }
 }
