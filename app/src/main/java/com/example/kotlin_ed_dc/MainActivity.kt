8ae1c6b86deeb2d25ae60671a1ac66e2990430aa
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-01-19 19:50:50 +0100

    Arreglado app

--- app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
@@ -8,20 +8,31 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    val bSumar = findViewById<Button>(R.id.bSumar);
-    val tvContador = findViewById<TextView>(R.id.tvMostrarValor);
+    // Hay que poner lateinit para decirle que no inicialice las variable
+    lateinit var bSumar: Button;
+    lateinit var tvContador: TextView;
+    var contador = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvContador.text = "Contador: 0";
+        // Extraer Views
+        bSumar = findViewById<Button>(R.id.bSumar);
+        tvContador = findViewById<TextView>(R.id.tvMostrarValor);
+        updateContadorText();
 
+        // Eventos
         bSumar.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Log.d("CLICKS:","PRUEBA")
+                contador++; updateContadorText();
             }
         })
     }
+
+    fun updateContadorText() {
+        tvContador.text = "Contador: $contador";
+    }
+
+
 }
