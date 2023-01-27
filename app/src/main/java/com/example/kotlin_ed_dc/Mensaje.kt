1a13cef726a61a442d8ce4bf58f94ec539ca6d29
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-01-27 15:49:51 +0100

    Merge

---
+++ app/src/main/java/com/example/kotlin_ed_dc/Mensaje.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_ed_dc
+
+//data class MensajeDC(
+//    var texto: String = "",
+//    var autor: String = "",
+//);
+//
+//class Mensaje {
+//    lateinit var texto: String;
+//    lateinit var autor: String;
+//
+//    constructor(texto: String = "", autor: String = "") {
+//        this.texto = texto; this.autor = autor;
+//    }
+//}
