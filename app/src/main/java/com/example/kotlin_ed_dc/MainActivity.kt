1dba52355b92f0f0076b96247ff8fd178a898790
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-01-27 16:28:01 +0100

    Ejemplo Beneficios Dataclases

--- app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
@@ -8,12 +8,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-// Dataclases
-data class MensajeDC(
-    var texto: String = "",
-    var autor: String = "",
-);
-
 class MainActivity : AppCompatActivity() {
     // Hay que poner lateinit para decirle que no inicialice las variable
     lateinit var bAniadirElemento: Button;
@@ -23,26 +17,37 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvAutor: TextView;
     lateinit var tvText: TextView;
 
+    lateinit var btnAdd: Button;
+
     lateinit var listMensajes: ArrayList<MensajeDC>;
 //    lateinit var _mapNumbers: Map<Int,String>;
 //    lateinit var _setNumbers: Set<Int>;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // Extraer Views
-
-
+        tvAutor = findViewById(R.id.etAutor);
+        tvText  = findViewById(R.id.etMensaje);
+        btnAdd  = findViewById(R.id.btAniadir);
 
         // Eventos
-
+        btnAdd.setOnClickListener(View.OnClickListener { addMensaje(); });
     }
 
 
     fun addMensaje() {
-        val msg = MensajeDC(tvAutor.text.toString(), tvText.text.toString());
-        listMensajes.add(msg);
+        var c_msg  = Mensaje  (tvAutor.text.toString(), tvText.text.toString());
+        var dc_msg = MensajeDC(tvAutor.text.toString(), tvText.text.toString());
+//        listMensajes.add(msg);
+
+        val TAG = "___DEBUGGING___";
+        //? Class
+        Log.i(TAG,"CLASS:     $c_msg");
+
+        //? Data Class
+        Log.i(TAG,"DATACLASS: $dc_msg")
     }
 
 
