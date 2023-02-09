5f8fb1e834f793d4c060ac279aac6d6072123e4a
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-02-09 19:35:30 +0100

    Progreso APP

--- app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ed_dc/MainActivity.kt
@@ -6,49 +6,72 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    // Hay que poner lateinit para decirle que no inicialice las variable
-    lateinit var bAniadirElemento: Button;
-    lateinit var bEliminarElemento: Button;
-    lateinit var bSiguienteElemento: Button;
-
+    // Hay que poner lateinit para decirle que no inicialice las variables
     lateinit var tvAutor: TextView;
     lateinit var tvText: TextView;
 
-    lateinit var btnAdd: Button;
+    lateinit var btnAdd:    Button;
+    lateinit var btnRemove: Button;
+    lateinit var btnNext: Button;
+    lateinit var btnPrev: Button;
 
-    lateinit var listMensajes: ArrayList<MensajeDC>;
-//    lateinit var _mapNumbers: Map<Int,String>;
-//    lateinit var _setNumbers: Set<Int>;
+
+    lateinit var rgroupDataStruct: RadioGroup;
+    var listMensajes: ArrayList<MensajeDC>   = ArrayList();
+    var setMensajes:  HashSet<MensajeDC>     = HashSet();
+    var mapMensajes:  HashMap<Int,MensajeDC> = HashMap();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Extraer Views
+        rgroupDataStruct = findViewById(R.id.rgroupDataStruct);
+        btnAdd     = findViewById(R.id.btAniadir);
+        btnRemove  = findViewById(R.id.btEliminar);
+        btnNext    = findViewById(R.id.btSig);
+        btnPrev    = findViewById(R.id.btAnt);
+
         tvAutor = findViewById(R.id.etAutor);
         tvText  = findViewById(R.id.etMensaje);
-        btnAdd  = findViewById(R.id.btAniadir);
 
         // Eventos
-        btnAdd.setOnClickListener(View.OnClickListener { addMensaje(); });
+        btnAdd.setOnClickListener { addMensaje() };
+        rgroupDataStruct.setOnCheckedChangeListener { _, checkedId -> loadMensajes(checkedId) };
     }
 
 
-    fun addMensaje() {
-        var c_msg  = Mensaje  (tvAutor.text.toString(), tvText.text.toString());
-        var dc_msg = MensajeDC(tvAutor.text.toString(), tvText.text.toString());
-//        listMensajes.add(msg);
+    private fun addMensaje() {
+        var msg = MensajeDC(tvAutor.text.toString(), tvText.text.toString());
 
-        val TAG = "___DEBUGGING___";
-        //? Class
-        Log.i(TAG,"CLASS:     $c_msg");
+        when (rgroupDataStruct.checkedRadioButtonId) {
+            R.id.rbList -> listMensajes.add(msg)
+            R.id.rbSet -> setMensajes.add(msg)
+            R.id.rbMap -> mapMensajes.set(mapMensajes.size, msg)
+            else -> Log.i("MainActivity.addMensaje()", "Not Supported")
+        }
+    }
 
-        //? Data Class
-        Log.i(TAG,"DATACLASS: $dc_msg")
+    private fun loadMensajes(checkedId: Int) {
+        // Reiniciar index
+        when (checkedId) {
+            R.id.rbList -> {
+                Log.i("MainActivity.loadMensajes()", "LIST" )
+            };
+            R.id.rbSet -> {
+                Log.i("MainActivity.loadMensajes()", "SET" )
+            };
+            R.id.rbMap -> {
+                Log.i("MainActivity.loadMensajes()", "MAP" )
+            };
+            else -> { Log.i("MainActivity.loadMensajes()", "NONE" ) };
+        }
     }
 
 
 }
+
